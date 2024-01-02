7fb5766b7f4111fcb5581a768f249620050fdfd6
Gonzavidal <gvidal1992@gmail.com>
2024-01-02 17:30:26 -0300

    Pokemon list shown, detail of each pokemon is missing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,7 +77,7 @@ dependencies {
     //implementation ("androidx.compose.material:material-icons-core:$compose_version")
     //implementation ("androidx.compose.material:material-icons-extended:$compose_version")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
-    implementation("androidx.navigation:navigation-compose:2.7.6")
+    //implementation("androidx.navigation:navigation-compose:2.7.6")  // Se comenta en función de la dependencia de Hilt para navegación
     implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
 
     // Retrofit
@@ -98,16 +98,16 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
 
     // Coil
-    implementation("io.coil-kt:coil:2.2.2")
+    implementation("io.coil-kt:coil-compose:2.2.2")
     implementation("com.google.accompanist:accompanist-coil:0.7.0")
 
-//Dagger - Hilt
+    //Dagger - Hilt
     implementation ("com.google.dagger:hilt-android:2.48")
     implementation ("com.google.dagger:dagger-android-support:2.48")   ///
     kapt ("com.google.dagger:hilt-android-compiler:2.48")
-    //kapt ("org.jetbrains.kotlinx:kotlinx-metadata-jvm:0.4.2")
+    kapt ("org.jetbrains.kotlinx:kotlinx-metadata-jvm:0.4.2")
     kapt ("androidx.hilt:hilt-compiler:1.0.0")
-    implementation ("androidx.hilt:hilt-navigation-compose:1.0.0")
+    implementation ("androidx.hilt:hilt-navigation-compose:1.1.0") //Se agrega en función de la dependencia de Hilt para navegación
 
     // Palette
     implementation("androidx.palette:palette-ktx:1.0.0")
